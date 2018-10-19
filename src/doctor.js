f59182a0810488f1021dd352cc242ef4b1e31182
Mark <mo.mangahas@yahoo.com>
2018-10-19 14:10:05 -0700

    finalizing the app

--- src/doctor.js
+++ src/doctor.js
@@ -1,10 +1,10 @@
 
 export class searchDoctor {
-    lookupDoctor(name, illness) {
+    currentStatus(name, illness) {
       return new Promise(function(resolve, reject) {
         let request = new XMLHttpRequest();
         console.log(name);
-        let url = `https://api.betterdoctor.com/2016-03-01/doctors?=name=${doctor}&query=${illness}&location=47.608013%2C-122.335167%2C100&user_location=47.608013%2C-122.336&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+        let url = `https://api.betterdoctor.com/2016-03-01/doctors?=name=${doctor}&query=${illness}&location=wa-seattle&skip=0&limit=10&user_key=${process.env.apiKey}`;
         request.onload = function() {
           if (this.status === 200) {
             console.log(request.response);
--- src/main.js
+++ src/main.js
@@ -5,29 +5,29 @@ import './styles.css';
 import { searchDoctor } from "./doctor.js";
 
 $(document).ready(function () {
-    $(document).ready(function () {
-        $("#doctorForm").submit(function (event) {
-            event.preventDefault();
-            $("#resultsArea").text("");
-            $("#error").text("");
-            let doctor = $("#doctorSearch").val();
-            let illness = $("#illnessSearch").val();
-            let newSearch = new searchDoctor();
+    $("#doctorForm").submit(function (event) {
+        event.preventDefault();
+        $("#results").text("");
+        $("#error").text("");
+        let doctor = $("#doctorSearch").val();
+        let illness = $("#illnessSearch").val();
+        let newSearch = new searchDoctor();
 
-            let promise = newSearch.lookupDoctor(doctor, illness);
-            promise.then(function (response) {
-                let body = JSON.parse(response);
-                if (body.data.length === 0) {
-                    $("#errorField").text("No doctor match search criterias.");
-                }
-                body.data.forEach(function (doctor) {
-                    $("#results").append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name} <br> </li> <br>`)
-                });
+        let promise = newSearch.currentStatus(doctor, illness);
+        promise.then(function (response) {
+            let body = JSON.parse(response);
+            if (body.data.length === 0) {
+                $("#error").text("No doctor found, try another one.");
+            }
+            body.data.forEach(function (doctor) {
+                $("#results").append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name} <br>
+                ${doctor.practices[0]} </li> <br>`)
+              });
 
-            }, function (error) {
-                $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-            });
+        }, function (error) {
+            $('.showErrors').text(`There was an error processing your request: ${error.message}`);
         });
+
 
     });
 });
