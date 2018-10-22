b8c262c4de4d4c212b5c16dbd7db01716fcb2bd1
moliver05 <41922658+moliver05@users.noreply.github.com>
2018-10-22 11:25:26 -0700

    Updated JS, working link

--- src/doctor.js
+++ src/doctor.js
@@ -1,13 +1,12 @@
+var Promise = require('es6-promise').Promise;
 
-export class searchDoctor {
-    currentStatus(name, illness) {
+export class Search{
+    foundDoctor(doctor) {
       return new Promise(function(resolve, reject) {
         let request = new XMLHttpRequest();
-        console.log(name);
-        let url = `https://api.betterdoctor.com/2016-03-01/doctors?=name=${doctor}&query=${illness}&location=wa-seattle&skip=0&limit=10&user_key=${process.env.apiKey}`;
+        let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${doctor}&location=wa-seattle&skip=0&limit=10&user_key=${process.env.API_KEY}`;
         request.onload = function() {
           if (this.status === 200) {
-            console.log(request.response);
             resolve(request.response);
           } else {
             console.log("error");
@@ -16,7 +15,25 @@ export class searchDoctor {
         }
         request.open("GET", url, true);
         request.send();
-        
+
+      });
+    }
+
+    foundIllness(illness) {
+      return new Promise(function(resolve, reject) {
+        let request = new XMLHttpRequest();
+        let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${illness}&location=wa-seattle&skip=0&limit=10&user_key=${process.env.API_KEY}`;
+        request.onload = function() {
+          if (this.status === 200) {
+            resolve(request.response);
+          } else {
+            console.log("error");
+            reject(Error(request.statusText));
+          }
+        }
+        request.open("GET", url, true);
+        request.send();
+
       });
     }
 }
